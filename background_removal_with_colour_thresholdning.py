3715e9bc7546ab64cacdeaedbddf29554fa55056
Silas Jensen <silasjensen2001@gmail.com>
2023-10-25 12:13:25 +0200

    More attempts to find yellow cones

--- background_removal_with_colour_thresholdning.py
+++ background_removal_with_colour_thresholdning.py
@@ -52,16 +52,19 @@ kernel3 = np.array([[0,0,0,0,0,0,0],
                     [0,0,0,0,0,0,0]], np.uint8)
 
 
-def colour_threshold_HSV(image, name: str, lower_val: list, upper_val: list):
-    # convert to HSV
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) 
+def colour_threshold(image, colourspace: str, lower_val: list, upper_val: list):
+    if colourspace == "HSV":
+        # convert to HSV
+        colour_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) 
+    elif colourspace == "BGR":
+        colour_image = image
 
     # set lower and upper colour limits
     temp_lower_val = np.array(lower_val)
     temp_upper_val = np.array(upper_val)
 
     # Threshold the HSV image to get only green colours
-    mask = cv2.inRange(hsv, temp_lower_val, temp_upper_val)
+    mask = cv2.inRange(colour_image, temp_lower_val, temp_upper_val)
 
     # apply mask to original image - this shows the green with black blackground
     only_green = cv2.bitwise_and(image, image, mask = mask)
@@ -184,11 +187,11 @@ cv2.destroyAllWindows()
 
 def find_blue_cones(image):
     # Find all blue parts of the cone using HSV colour thresholding
-    img_blue = colour_threshold_HSV(image, "img1", [80,95,110], [165,255,255])
+    img_blue = colour_threshold(image, "HSV", [80,95,110], [165,255,255])
     
     # Find all the white lines on the cones by inverting the image to easily detect white colours with HSV colour thresholding
     inv_image = 255-image
-    img_white_lines = colour_threshold_HSV(inv_image, "img2", [0,0,0], [255,255,55])
+    img_white_lines = colour_threshold(inv_image, "HSV", [0,0,0], [255,255,55])
 
     # Convert the images to greyscale to convert them to binary images
     gray_img_blue = cv2.cvtColor(img_blue, cv2.COLOR_BGR2GRAY)
@@ -217,34 +220,99 @@ def find_yellow_cones(image):
     enhance_img = enhance_contrast(image)
 
     gray_enhance_img = cv2.cvtColor(enhance_img, cv2.COLOR_BGR2GRAY)
-    print(gray_enhance_img.shape)
+
     # Apply adaptive thresholding
     # You can choose from different methods like cv2.ADAPTIVE_THRESH_MEAN_C or cv2.ADAPTIVE_THRESH_GAUSSIAN_C
     # Block size defines the size of the neighborhood area for threshold calculation
     # C is a constant subtracted from the mean (or weighted mean in the case of Gaussian) value
     #result = cv2.adaptiveThreshold(gray_enhance_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, blockSize=3, C=6)
     
-    img_yellow = colour_threshold_HSV(enhance_img, "img1", [20,95,110], [35,255,255])
+    img_yellow = colour_threshold(enhance_img, "HSV", [20,95,110], [35,255,255])
 
     inv_image = 255-image
-    img_black_lines = colour_threshold_HSV(enhance_img, "img2", [10,55,120], [255,255,255]) #[0,0,0], [255,255,255]
+    img_black_lines = colour_threshold(enhance_img, "HSV", [10,75,0], [75,255,255]) #[0,0,0], [255,255,255], (enhance_img, "BGR", [50,55,120], [255,255,255])
     
     # Creating grayscale images
     gray_img_yellow = cv2.cvtColor(img_yellow, cv2.COLOR_BGR2GRAY)
     gray_img_black = cv2.cvtColor(img_black_lines, cv2.COLOR_BGR2GRAY)
     
-    result = cv2.adaptiveThreshold(gray_img_black, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, blockSize=3, C=6)
+    result = cv2.adaptiveThreshold(gray_img_black, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, blockSize=13, C=2)
     inv_result = 255-result
 
     # Set up a threshold mark for making binary images
     _, binary_img_yellow = cv2.threshold(gray_img_yellow, 245, 255, cv2.THRESH_BINARY)
     inv_binary_img_yellow = 255-binary_img_yellow
+    eroded_result = cv2.dilate(result, kernel2, iterations= 1)
+    background = np.zeros(eroded_result.shape, eroded_result.dtype)
+    hej = cv2.bitwise_xor(eroded_result, inv_result, inv_result)
 
-    cv2.imshow("Result", result)
-    cv2.imshow("Yellow Cones", binary_img_yellow)
-    #cv2.imshow("Black Cones", img_black_lines)
+    #cv2.imshow("Something", hej)
+    #cv2.imshow("Result", inv_result)
+    #cv2.imshow("Yellow Cones", binary_img_yellow)
+    cv2.imshow("Black Cones", img_black_lines)
 
+def find_yellow_cones_with_Hough(image):
+    enhance_img = enhance_contrast(image)
+    # 1. Load the image (assuming you already have the isolated yellow cones)
+    img_yellow = colour_threshold(enhance_img, "HSV", [20,95,110], [35,255,255])
+    min_area = 100
+    # 2. Convert to grayscale
+    gray = cv2.cvtColor(img_yellow, cv2.COLOR_BGR2GRAY)
 
-find_yellow_cones(resized_image_2)
+    # 3. Apply edge detection
+    edges_1 = cv2.Canny(gray, 0, 250)  # You can adjust the thresholds as needed
+
+    # 4. Post-processing (erosion and dilation)
+    kernel = np.ones((3, 3), np.uint8)
+    edges_2 = cv2.dilate(edges_1, kernel, iterations=1)
+    edges_3 = cv2.erode(edges_2, kernel, iterations=1)
+
+    # 5. Contour Detection
+    contours, _ = cv2.findContours(edges_3, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # 6. Iterate through detected contours
+    for contour in contours:
+        # You can add additional filtering based on contour area, aspect ratio, etc.
+        if cv2.contourArea(contour) > min_area:  # Adjust min_area as needed
+            # Get the bounding box of the contour
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(img_yellow, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    # 7. Line Detection using Hough Line Transform
+    lines = cv2.HoughLines(edges_3, 1, np.pi / 180, threshold=200)
+
+    if lines is not None:
+        for line in lines:
+            rho, theta = line[0]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
+            cv2.line(img_yellow, (x1, y1), (x2, y2), (0, 0, 255), 2)  # Draw the detected lines
+
+    cv2.imshow('Dark Lines', img_yellow)
+    cv2.imshow('Edges', edges_3)
+
+def find_dark_lines_in_yellow_cones(image):
+    # 1. Load the image
+    enhance_img = enhance_contrast(image)
+    img_yellow = colour_threshold(enhance_img, "HSV", [20, 95, 110], [35, 255, 255])
+
+    # 2. Convert to grayscale and ensure the correct data type (usually CV_8U)
+    gray = cv2.cvtColor(img_yellow, cv2.COLOR_BGR2GRAY)
+    
+    edges = cv2.Canny(gray, 0, 250)  # Adjust thresholds as needed
+
+    # Convert the result to the appropriate data type
+    #laplacian = np.uint8(np.absolute(laplacian))
+
+    cv2.imshow('Yellow Cones', edges)
+    #cv2.imshow('Dark Lines', edges)
+
+find_dark_lines_in_yellow_cones(resized_image_2)
 cv2.waitKey()
 cv2.destroyAllWindows()
